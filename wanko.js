0b6b83669c46d38f78946286117dfdc42ba69fca
minayoco <minamiyokoyama0913@gmail.com>
2021-08-16 23:52:12 +0900

    変更

--- wanko.js
+++ wanko.js
@@ -1,3 +1,15 @@
 
-console.log("わんこ");
 
+/* ハンバーガーボタン */
+$(function() {
+    $('.hamburger').click(function() {
+        $(this).toggleClass('active');
+ 
+        if ($(this).hasClass('active')) {
+            $('.header-list').addClass('active');
+        } else {
+            $('.header-list').removeClass('active');
+        }
+    });
+});
+/* ハンバーガーボタン */
